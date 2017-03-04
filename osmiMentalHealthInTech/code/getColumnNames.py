b29b33537b615c4a7eef5ecdceb92f1f618d04fa
PLBMR <mmrosenb@andrew.cmu.edu>
2017-03-04 17:48:23 -0500

    Added some more eda

---
+++ osmiMentalHealthInTech/code/getColumnNames.py
@@ -0,0 +1,12 @@
+#getColumnNames.py
+#Quick subroutine to list out all relevant column names of the data frame
+
+#imports
+
+import pandas as pd
+
+#run method
+
+responseFrame = pd.read_csv("../data/osmi-survey-2016_data.csv")
+for columnName in responseFrame.columns:
+    print columnName
