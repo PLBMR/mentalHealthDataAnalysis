6ab2af74f4ab8f427af62b7c154e60828cb8be19
PLBMR <mmrosenb@andrew.cmu.edu>
2017-10-28 19:11:52 -0400

    Changed some code to account for the separation between comments and posts

---
+++ rDepression/code/preprocess_filter.py
@@ -0,0 +1,36 @@
+#preprocess.py
+#script for filtering our dataset and tokenizing our sentences
+
+#imports
+
+import pandas as pd
+import json
+from nltk.tokenize import word_tokenize as wt
+
+#helpers
+
+def processData(rawFrame):
+    #main process for processing our dataset
+    #first, simplify our analysis to just the relevant variables
+    relVars = ["body","id","link_id","parent_id"]
+    relVarFrame = rawFrame[relVars]
+    #then filter out Null or deleted comments
+    filteredFrame = relVarFrame[relVarFrame["body"].notnull()]
+    filteredFrame = filteredFrame[~(filteredFrame["body"].isin([
+                                    "[removed]","[deleted]"]))]
+    #tokenize sentences
+    sentDict = {row[1]["id"]:wt(row[1]["body"].decode("utf8")) 
+                for row in filteredFrame.iterrows()}
+    #prepare idFrame
+    idFrame = filteredFrame[["id","link_id","parent_id"]]
+    return idFrame, sentDict
+
+#main process
+
+if __name__ == "__main__":
+    rawFrame = pd.read_csv("../data/raw/allRDepressionData.csv")
+    idFrame, sentDict = processData(rawFrame)
+    idFrame.to_csv("../data/preprocessed/idFrame.csv",index = False)
+    indentNum = 4
+    json.dump(sentDict,open("../data/preprocessed/sentDict.json","wb"),
+              indent = indentNum)
